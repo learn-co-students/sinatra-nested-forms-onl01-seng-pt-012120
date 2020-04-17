04a8882604ae02631009e2ee6fa8fee7cc812787
Daniel Sun <danielsun12345@gmail.com>
2020-04-17 23:41:22 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  
+  @@ships = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+  end 
+  
+  def self.all 
+    @@ships
+  end 
+  
+  def self.clear
+    @@ships.clear
+  end 
+  
 end
