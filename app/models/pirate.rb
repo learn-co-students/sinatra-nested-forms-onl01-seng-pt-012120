115a745a82bde3bb22bf7bf0ef2ffd50c7c114fd
Harley McDowell <hmcdowell95@gmail.com>
2020-04-26 20:21:06 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:weight]
+    @height = arg[:height]
+    @@all << self
+  end
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end
+  
 end
